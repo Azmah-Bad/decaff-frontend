a37791c9768b49b8ba6fb0cd406086773c435b3f
Azmah-Bad <56384031+Azmah-Bad@users.noreply.github.com>
2020-07-21 13:51:31 +0200

    skeleton app with the home page and the tabs (no logic tho)
    Signed-off-by: Azmah-Bad <56384031+Azmah-Bad@users.noreply.github.com>

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  caffeineIntakeToday = 0;
+  private cup2caffeine = { "espresso": 64, "coke": 22 } // in mg
 
   constructor() {}
+
+  LogCuteCoffee(cup: string): void {
+    this.caffeineIntakeToday += this.cup2caffeine[cup]
+    // this.coffeeService.intake(this.cup2caffeine[cup])
+    // Dancing coffee easter egg
+    if (this.caffeineIntakeToday > 200) {
+      // this.audioService.play();
+      // this.nativeAudio.play('TEST')
+      
+    }
+  }
 
 }
